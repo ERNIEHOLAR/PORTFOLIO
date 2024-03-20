e9052e466ea58048f0dd121c97b01a0e64e764e2
ernie <ogunbanwoeniola.oe@gmail.com>
2024-03-20 20:56:38 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+
+
+
+<li><a href="index.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="project.html">Projects</a></li>
+        <li><a href="contact.html">Contact</a></li>
+
+        0% {
+            clip-path: inset(29% 0px 25%);
+        }
+
+        5% {
+            clip-path: inset(9% 0px 38%);
+        }
+
+        10% {
+            clip-path: inset(96% 0px 1%);
+        }
+
+        15% {
+            clip-path: inset(75% 0px 23%);
+        }
+
+        20% {
+            clip-path: inset(46% 0px 50%);
+        }
+
+        25% {
+            clip-path: inset(3% 0px 46%);
+        }
+
+        30% {
+            clip-path: inset(82% 0px 2%);
+        }
+
+        35% {
+            clip-path: inset(88% 0px 1%);
+        }
+
+        40% {
+            clip-path: inset(91% 0px 8%);
+        }
+
+        45% {
+            clip-path: inset(96% 0px 2%);
+        }
+
+        50% {
+            clip-path: inset(59% 0px 38%);
+        }
+
+        55% {
+            clip-path: inset(41% 0px 53%);
+        }
+
+        60% {
+            clip-path: inset(21% 0px 47%);
+        }
+
+        65% {
+            clip-path: inset(89% 0px 4%);
+        }
+
+        70% {
+            clip-path: inset(1% 0px 95%);
+        }
+
+        75% {
+            clip-path: inset(86% 0px 4%);
+        }
+
+        80% {
+            clip-path: inset(95% 0px 5%);
+        }
+
+        85% {
+            clip-path: inset(54% 0px 36%);
+        }
+
+        90% {
+            clip-path: inset(70% 0px 27%);
+        }
+
+        95% {
+            clip-path: inset(6% 0px 16%);
+        }
+        
+        100% {
+            clip-path: inset(95% 0px 2%);
+        }
+         
