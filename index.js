e1ac66fa752e12f60331d9e1e20ee079c341f73e
ernie <ogunbanwoeniola.oe@gmail.com>
2024-04-05 05:30:38 +0100

    profile picture added

--- index.js
+++ index.js
@@ -1,92 +1,28 @@
-
-
-
-<li><a href="index.html">Home</a></li>
-        <li><a href="about.html">About</a></li>
-        <li><a href="project.html">Projects</a></li>
-        <li><a href="contact.html">Contact</a></li>
-
-        0% {
-            clip-path: inset(29% 0px 25%);
-        }
-
-        5% {
-            clip-path: inset(9% 0px 38%);
-        }
-
-        10% {
-            clip-path: inset(96% 0px 1%);
-        }
-
-        15% {
-            clip-path: inset(75% 0px 23%);
-        }
-
-        20% {
-            clip-path: inset(46% 0px 50%);
-        }
-
-        25% {
-            clip-path: inset(3% 0px 46%);
-        }
-
-        30% {
-            clip-path: inset(82% 0px 2%);
-        }
-
-        35% {
-            clip-path: inset(88% 0px 1%);
-        }
-
-        40% {
-            clip-path: inset(91% 0px 8%);
-        }
-
-        45% {
-            clip-path: inset(96% 0px 2%);
-        }
-
-        50% {
-            clip-path: inset(59% 0px 38%);
-        }
-
-        55% {
-            clip-path: inset(41% 0px 53%);
-        }
-
-        60% {
-            clip-path: inset(21% 0px 47%);
-        }
-
-        65% {
-            clip-path: inset(89% 0px 4%);
-        }
-
-        70% {
-            clip-path: inset(1% 0px 95%);
-        }
-
-        75% {
-            clip-path: inset(86% 0px 4%);
-        }
-
-        80% {
-            clip-path: inset(95% 0px 5%);
-        }
-
-        85% {
-            clip-path: inset(54% 0px 36%);
-        }
-
-        90% {
-            clip-path: inset(70% 0px 27%);
-        }
-
-        95% {
-            clip-path: inset(6% 0px 16%);
-        }
-        
-        100% {
-            clip-path: inset(95% 0px 2%);
-        }
-         
+const body = document.querySelector("body");
+const btn = document.getElementById("btn_one");
+const icon = document.getElementById("moon");
+function changeTheme() {
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    btn.style.backgroundColor = "#fff";
+    icon.style.color = "#0d151c";
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-sun");
+  } else {
+    btn.style.backgroundColor = "#0d151c";
+    icon.style.color = "#fff";
+    icon.classList.remove("fa-sun");
+    icon.classList.add("fa-moon");
+  }
+}
+btn.addEventListener("click", changeTheme);
+let prevScrollpos = window.scrollY;
+window.onscroll = function () {
+  let currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("lg-nav").style.top = "0";
+  } else {
+    document.getElementById("lg-nav").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+};
