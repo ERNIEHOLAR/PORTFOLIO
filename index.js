270266e00e0deb0462029f1415c8a90a2bd774b8
ernie <ogunbanwoeniola.oe@gmail.com>
2024-04-11 15:47:19 +0100

    dark-mode

--- index.js
+++ index.js
@@ -1,28 +1,72 @@
-const body = document.querySelector("body");
-const btn = document.getElementById("btn_one");
-const icon = document.getElementById("moon");
-function changeTheme() {
-  body.classList.toggle("dark");
-  if (body.classList.contains("dark")) {
-    btn.style.backgroundColor = "#fff";
-    icon.style.color = "#0d151c";
-    icon.classList.remove("fa-moon");
-    icon.classList.add("fa-sun");
-  } else {
-    btn.style.backgroundColor = "#0d151c";
-    icon.style.color = "#fff";
-    icon.classList.remove("fa-sun");
-    icon.classList.add("fa-moon");
+
+/**
+* Utility function to calculate the current theme setting.
+* Look for a local storage value.
+* Fall back to system setting.
+* Fall back to light mode.
+*/
+function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+  if (localStorageTheme !== null) {
+    return localStorageTheme;
   }
+
+  if (systemSettingDark.matches) {
+    return "dark";
+  }
+
+  return "light";
 }
-btn.addEventListener("click", changeTheme);
-let prevScrollpos = window.scrollY;
-window.onscroll = function () {
-  let currentScrollPos = window.scrollY;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("lg-nav").style.top = "0";
-  } else {
-    document.getElementById("lg-nav").style.top = "-100px";
-  }
-  prevScrollpos = currentScrollPos;
-};
+
+/**
+* Utility function to update the button text and aria-label.
+*/
+function updateButton({ buttonEl, isDark }) {
+  const newCta = isDark ? "Change to light theme" : "Change to dark theme";
+  // use an aria-label if you are omitting text on the button
+  // and using a sun/moon icon, for example
+  buttonEl.setAttribute("aria-label", newCta);
+  buttonEl.innerText = newCta;
+}
+
+/**
+* Utility function to update the theme setting on the html tag
+*/
+function updateThemeOnHtmlEl({ theme }) {
+  document.querySelector("html").setAttribute("data-theme", theme);
+}
+
+
+/**
+* On page load:
+*/
+
+/**
+* 1. Grab what we need from the DOM and system settings on page load
+*/
+const button = document.querySelector("[data-theme-toggle]");
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+/**
+* 2. Work out the current site settings
+*/
+let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
+
+/**
+* 3. Update the theme setting and button text accoridng to current settings
+*/
+updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
+updateThemeOnHtmlEl({ theme: currentThemeSetting });
+
+/**
+* 4. Add an event listener to toggle the theme
+*/
+button.addEventListener("click", (event) => {
+  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+  localStorage.setItem("theme", newTheme);
+  updateButton({ buttonEl: button, isDark: newTheme === "dark" });
+  updateThemeOnHtmlEl({ theme: newTheme });
+
+  currentThemeSetting = newTheme;
+}); 
